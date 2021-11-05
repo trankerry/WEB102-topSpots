a1223e34fd7335a9687502dae58386b41060b677
Kerry <trankerry94@gmail.com>
2021-11-05 12:00:01 -0700

    tests completed -- test may have a bug

--- main.js
+++ main.js
@@ -1,4 +1,25 @@
-$(document).ready(function() {
-// write your code here
+$(document).ready(function () {
+  //write your code here
+  $.getJSON('data.json', function (data) {
 
+    let table = document.getElementById('table');
+
+    data.forEach(dataset => {
+    //create new row elements
+      let newRow = table.insertRow();
+      //create new columns elements
+      let newName = newRow.insertCell();
+      let newDescription = newRow.insertCell();
+      let newLocation = newRow.insertCell();
+
+      let latitude = dataset.location[0];
+      let longitude = dataset.location[1];
+      let link = `https://www.google.com/maps?q=${latitude},${longitude}`
+
+      newName.innerHTML = `${dataset.name}`
+      newDescription.innerHTML = `${dataset.description}`
+      newLocation.innerHTML = `<a href="${link}" target="_blank"><button>Open in Google Maps</button></a>`;
+
+    });
+  })
 });
